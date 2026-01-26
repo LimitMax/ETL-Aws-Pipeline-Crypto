a5c29487c86dc4f278cd9bfcdcc81f8a1ee81ebc
LimitMax <taufiqqurohman98@gmail.com>
2026-01-26 08:41:34 +0700

    update config

--- silver/config.py
+++ silver/config.py
@@ -3,5 +3,5 @@ import os
 BRONZE_BUCKET = os.environ.get("BRONZE_BUCKET")
 SILVER_BUCKET = os.environ.get("SILVER_BUCKET", BRONZE_BUCKET)
 
-BRONZE_PREFIX = os.environ.get("BRONZE_PREFIX", "bronze/crypto")
+BRONZE_PREFIX = os.environ.get("BRONZE_PREFIX", "bronze/crypto/asset_type=crypto/source=yfinance")
 SILVER_PREFIX = os.environ.get("SILVER_PREFIX", "silver/crypto")
